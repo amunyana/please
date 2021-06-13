299832715843f3f9cf77b008ce65849ed6df0bb0
amunyana <67626756+amunyana@users.noreply.github.com>
2021-06-13 02:37:46 -0700

    Update pleasescript.js

--- pleasescript.js
+++ pleasescript.js
@@ -16,8 +16,8 @@ const songs = [
   {
     title: 'Please',
     artist: 'Maya-Marie',
-    coverPath: 'assets/images/Almiraj.png',
-    discPath: 'assets/music/Please.mp3',
+    coverPath: 'Almiraj.png',
+    discPath: 'Please.mp3',
     duration: '0:01',
   },
 ];
@@ -119,7 +119,7 @@ play.addEventListener('click', playPauseMedia);
 disc.addEventListener('play', updatePlayPauseIcon);
 disc.addEventListener('pause', updatePlayPauseIcon);
 disc.addEventListener('timeupdate', updateProgress);
-disc.addEventListener('ended', gotoNextSong.bind(null, true));
+disc.addEventListener('ended');
 
 // Go to next song when next button clicked
 prev.addEventListener('click', gotoPreviousSong);
